ad8acfac553fd5658100f127e069a8537a2e1312
Virgile Patrignani <virgile_patrignani@hotmail.com>
2019-04-10 19:49:03 +0000

    added componentMount, and third message

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,25 @@ const chattyDefaultData = {
   ]
 }
 
+
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = chattyDefaultData;
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+      // const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages:[...this.state.messages, newMessage]})
+    }, 3000);
   }
 
   render() {
