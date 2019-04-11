23989e1fffc46e89ab3139748567b947c278efd2
Virgile Patrignani <virgile_patrignani@hotmail.com>
2019-04-11 16:46:08 +0000

    created and setup webSocket server. Modified app.jsx in componentDidMount to listen to websocket server

--- src/App.jsx
+++ src/App.jsx
@@ -18,41 +18,32 @@ const chattyDefaultData = {
   ]
 }
 
-
-/* 1. Function we passed through props to the ChatBar component is called ...
-   2.this.setState is called with a new array that contains a new message
-   3. Will trigger React to call our componenets render() method
-   4. the new state is passed to all children of the App component
-*/
-
-
-
-
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = chattyDefaultData;
+
+
+
   }
   componentDidMount() {
-    console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-      this.setState({messages:[...this.state.messages, newMessage]})
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-    }, 3000);
+    const socketUrl = 'ws://localhost:3001'
+    
+    this.socket = new WebSocket(socketUrl);
+    this.socket.onopen = this.handleOnOpen;
+    this.socket.onmessage= this.handleOnMessage;
+    this.socket.onerror = this.handleOnError;
+    
   }
   // function i call when the messages are submitted. Create object like newmessage, add to state.
-    handleInsertMessage = (content, username) => {
-      const newMessage = {
-        id: this.state.messages.length + 1,
-        username: username,
-        content: content
-      };
-      this.setState({messages:[...this.state.messages, newMessage]})
+  handleInsertMessage = (content, username) => {
+    const newMessage = {
+      id: this.state.messages.length + 1,
+      username: username,
+      content: content
+    };
+    this.setState({messages:[...this.state.messages, newMessage]})
     }
   render() {
     return (
